9aa6df8c61dce9a98e1504434909210d604ed8a2
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2017-12-23 23:01:49 +0100

    no CSP, breaks CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,11 +33,11 @@ module.exports = {
     {  // make sure to put last in the array
      resolve: `gatsby-plugin-netlify`,
      options: {
-       headers: {
+       /* headers: {
         "/*": [
           "Content-Security-Policy: default-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
         ],
-      },
+      }, */
        //headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
        //allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers (disabled by default, until gzip is fixed for server push)
