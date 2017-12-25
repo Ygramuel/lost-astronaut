4bae8b3e6019886b6e549e589d8b469127ce6066
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2017-12-25 19:35:21 +0100

    svg-logo in header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,12 +4,14 @@ import Menu from "../components/Menu/Menu"
 import style from "./Header.module.less"
 
 import data from './header.json'
+import logo from '../img/LostAstronaut.svg'
 
 export const Header = ({  }) => {
   return (
     <header>
-      {/* TODO replace this with a picture */}
-      <Link to='/'>{data.title}</Link>
+      <Link to='/'>
+        <img src={logo} alt='Home'/>
+      </Link>
       <Menu elements={data.menu}/>
     </header>
   );
