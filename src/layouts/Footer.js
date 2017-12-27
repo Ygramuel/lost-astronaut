142c7647b3b982760b62fcc9d4a878506d8e0a5b
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2017-12-27 19:48:06 +0100

    footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,10 +1,27 @@
 import React from "react";
+
+import Menu from "../components/Menu/Menu"
+
 import style from "./Footer.module.less"
+import data from "./footer.json"
 
 export const Footer = ({  }) => {
   return (
     <footer>
-    <p>Footer</p>
+      <div>
+        <h6>{data.left.title}</h6>
+        <div>{data.left.text}</div>
+      </div>
+      
+      <div>
+        <h6>{data.middle.title}</h6>
+        <Menu elements={data.middle.nav} />
+      </div>
+
+      <div>
+        <h6>{data.right.title}</h6>
+        <a href={data.right.facebook}>FACEBOOK</a>
+      </div>
     </footer>
   );
 };
