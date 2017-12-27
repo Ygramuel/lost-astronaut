dacc8999e5ec1e9df43936559c0e67f36cb1ecad
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2017-12-27 17:09:33 +0100

    startseite weiter

--- src/templates/index-page/index-page.js
+++ src/templates/index-page/index-page.js
@@ -3,8 +3,11 @@ import Content, { HTMLContent } from '../../components/Content';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link'
 
+import Feature from './Feature'
+import Zeile from './Zeile'
+
 export const IndexPageTemplate =
-  ({ title, content, image, slogans, kunden, box}) => {
+  ({ title, content, image, slogan, kunden, box, features, mockupImage, kundenTitle}) => {
   return (
     <div>
       <Helmet>
@@ -12,15 +15,35 @@ export const IndexPageTemplate =
       </Helmet>
       <h1>{title}</h1>
       <img src={image}/>
+
       <div>
-        {slogans.map((slogan) =>
-          <p>{slogan.slogan}</p>
-        )}
+        <h6>{slogan.title}</h6>
+        <p>{slogan.text}</p>
       </div>
+
+      {/* Alle Feature Elemente */}
+      <Zeile
+        elements=
+          {features.map((feature) =>
+            <Feature  text={feature.text}
+                      title={feature.title}
+                      image={feature.image} />
+        )}/>
+
+      <img src={mockupImage}/>
+
+      {/* Profil */}
       <div>
+        <h5>{box.title}</h5>
+        <p>{box.text}</p>
+      </div>
+
+      {/* Alle Kunden Elemente */}
+      <div>
+        <h5>{kundenTitle}</h5>
         {kunden.map((kunde) =>
           <a href={kunde.url}>
-            <img src={kunde.bild}/>
+            <img src={kunde.image}/>
             <p>
               {kunde.name}
             </p>
@@ -39,19 +62,19 @@ export default ({ data }) => {
   return <IndexPageTemplate
     title={post.frontmatter.title}
     image={post.frontmatter.image}
-    slogans={post.frontmatter.slogans}
-    kunden={post.frontmatter.kunden}
-    content={<HTMLContent content={post.html} />}
+    slogan={post.frontmatter.slogan}
+    features={post.frontmatter.features}
     box={post.frontmatter.box}
+    mockupImage={post.frontmatter.mockupimage}
+    kundenTitle={post.frontmatter.kunden.title}
+    kunden={post.frontmatter.kunden.kunde}
   />;
 };
 
 export const indexPageQuery = graphql`
   query IndexPage($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
       frontmatter {
-        path
         title
         image
         slogan{
@@ -63,16 +86,25 @@ export const indexPageQuery = graphql`
           image
           text
         }
-        mockupImage
+        mockupimage
         box {
           title
           text
         }
-        kunden {
-          bild
-          name
+        portfolios{
+          title
           url
+          image
         }
+        kunden{
+          title
+          kunde{
+            name
+            image
+            url
+          }
+        }
+
       }
     }
   }
