851d03f1a0cf1dd7f92c0fa89577734ded54152c
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2017-12-31 00:06:05 +0100

    more form validation and autocomplete

--- src/components/KontaktForm/KontaktForm.js
+++ src/components/KontaktForm/KontaktForm.js
@@ -1,15 +1,24 @@
 import React from "react"
 import style from "./KontaktForm.module.less"
 
+// Read
+// https://developers.google.com/web/fundamentals/design-and-ux/input/forms/
+
 export default () =>
     <form className={style.form} name="contact" data-netlify="true">
+        <label for="name" className={style.name}>
+          <p>Name</p>
+          <input type="text" name="name" placeholder="Max Mustermann" minlength="4" required autocomplete="name"/>
+        </label>
 
-        <label className={style.name}><p>Name</p><input type="text" name="name"/></label>
+        <label for="email" className={style.mail}>
+          <p>E-Mail</p>
+          <input type="email" name="email" placeholder="name@example.com" minlength="4" required autocomplete="email"/>
+        </label>
 
-        <label className={style.mail}><p>E-Mail</p><input type="email" name="email"/></label>
-
-        <label className={style.message}><p>Nachricht</p><textarea name="message"></textarea></label>
-
+        <label for="text" className={style.message}>
+          <p>Nachricht</p>
+          <textarea name="text" required minlength="10"></textarea>
+        </label>
         <button className={style.button} type="submit">Absenden</button>
-
     </form>
