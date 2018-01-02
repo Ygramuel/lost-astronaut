787712668fcd09d4e8577db0f079037e167fcbe9
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-01-02 22:22:53 +0100

    CSP enabled

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,16 +41,19 @@ module.exports = {
         display: "minimal-ui",
       }
     },
-    'gatsby-plugin-offline', 
+    'gatsby-plugin-offline',
 
     {  // make sure to put last in the array
      resolve: `gatsby-plugin-netlify`,
      options: {
-       /* headers: {
+        headers: {
         "/*": [
           "Content-Security-Policy: default-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
         ],
-      }, */
+        "/admin/*": [
+          "Content-Security-Policy: default-src *"
+        ],
+      },
        //headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
        //allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers (disabled by default, until gzip is fixed for server push)
