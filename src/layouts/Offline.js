0549bbf5c36bbe6a29f7356e80ab89ebd832faf1
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-01-05 00:23:40 +0100

    Offline display message

---
+++ src/layouts/Offline.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class Offline extends React.Component {
+  constructor() {
+      super()
+
+      // I guess in the beginning we are online ...
+      this.state = {
+        online: true,
+      }
+    }
+
+    componentDidMount() {
+      // TODO is there a better way?
+      // e.g. "bind()" ??
+      var that = this
+      // Update state if we go online or offline
+      function updateOnlineStatus(event) {
+        that.setState({ online: navigator.onLine })
+      }
+
+      window.addEventListener('online',  updateOnlineStatus);
+      window.addEventListener('offline', updateOnlineStatus);
+    }
+
+    render() {
+      const { online } = this.state
+
+      return (
+        <div>
+          {online ? <h1>ONLINE</h1> :<h1>OFFLINE</h1>}
+        </div>
+      )
+    }
+}
+
+export default Offline;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,8 @@ import Helmet from 'react-helmet';
 
 import Header from "./Header.js"
 import Footer from "./Footer.js"
+import Offline from "./Offline.js"
+
 import style from "./index.module.less"
 import './reset.less';
 import './font.less';
@@ -12,11 +14,12 @@ import './font.less';
 const TemplateWrapper = ({ children }) => (
   <div className={style.wrapper}>
     <Helmet
-    //  titleTemplate="%s | MyAwesomeWebsite.com" 
+    //  titleTemplate="%s | MyAwesomeWebsite.com"
     >
       <html lang="de" />
     </Helmet>
     <Header />
+    <Offline />
     <div className={style.content}>{children()}</div>
     <Footer />
   </div>
