41927ca3570e48d39eac16093bb863e67ead4f07
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-01-04 23:46:29 +0100

    autoscroll

--- src/pages/Slider.js
+++ src/pages/Slider.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Slider from 'react-slick'
+
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
@@ -10,6 +11,8 @@ class SimpleSlider extends React.Component {
       dots: true,
       infinite: true,
       speed: 500,
+      autoplay: true,
+      autoplaySpeed: 5000,
       slidesToShow: 1,
       slidesToScroll: 1
     };
