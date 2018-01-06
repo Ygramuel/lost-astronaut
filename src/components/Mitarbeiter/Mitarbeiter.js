3469abf746bed005a195bf1224c4c265bae9d33a
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-01-06 20:09:54 +0100

    mail icon for mitarbeiter

--- src/components/Mitarbeiter/Mitarbeiter.js
+++ src/components/Mitarbeiter/Mitarbeiter.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Link from "gatsby-link"
+
 import style from "./Mitarbeiter.module.less"
+
+import mailIcon from './mail.svg'
 
 export const Mitarbeiter = ({ name, mail, text, bild }) => {
   bild = encodeURI(bild);
@@ -9,7 +12,9 @@ export const Mitarbeiter = ({ name, mail, text, bild }) => {
       <img src={bild} />
       <h5 className={style.name}>{name}</h5>
       <p className={style.function}>{text}</p>
-      <a href={"mailto:" + mail}>{mail}</a>
+      <a href={"mailto:" + mail}>
+        <img src={mailIcon} alt={mail} />
+      </a>
     </div>
   );
 };
