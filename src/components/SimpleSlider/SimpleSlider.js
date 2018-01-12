3ecfec4aa0cb973668d1c6a5430317abcd4b75ba
Ygramuel <JhN45D84QM6fYQhd7BCLMeEf>
2018-01-12 22:35:01 +0100

    Sliderkram ohne was geändert zu haben

--- src/components/SimpleSlider/SimpleSlider.js
+++ src/components/SimpleSlider/SimpleSlider.js
@@ -3,8 +3,7 @@ import React from "react";
 // https://github.com/leandrowd/react-responsive-carousel
 import { Carousel } from 'react-responsive-carousel'
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-
-// import style from "./Slider.module.less"
+import style from "./Slider.module.less"
 
 class SimpleSlider extends React.Component {
   render() {
