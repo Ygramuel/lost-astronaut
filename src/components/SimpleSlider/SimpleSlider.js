520c69b00e7d61ec9d287366fee47de6229f0d6f
Ygramuel <JhN45D84QM6fYQhd7BCLMeEf>
2018-01-12 23:13:26 +0100

    Slider

--- src/components/SimpleSlider/SimpleSlider.js
+++ src/components/SimpleSlider/SimpleSlider.js
@@ -3,12 +3,12 @@ import React from "react";
 // https://github.com/leandrowd/react-responsive-carousel
 import { Carousel } from 'react-responsive-carousel'
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-import style from "./Slider.module.less"
+import style from "./Slider.less"
 
 class SimpleSlider extends React.Component {
   render() {
     return (
-      <Carousel dynamicHeight={false} autoPlay={true}>
+      <Carousel className="slider" dynamicHeight={false} autoPlay={true}>
         {this.props.children}
       </Carousel>
     );
