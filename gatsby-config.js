a0b5efd3001739f39625ce4ed69a33d3ea684cab
Sebastian Stüber <sebastian.stueber@rwth-aachen.de>
2018-03-25 11:41:45 +0200

    favicons are there

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,18 @@ module.exports = {
         theme_color: "#ff6666",
         lang: "de",
         display: "standalone",
+        icons: [
+            {
+                src: "/favicons/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                src: "/favicons/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
+            }
+        ],
       }
     },
     'gatsby-plugin-offline',
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -20,6 +20,16 @@ const TemplateWrapper = ({ children }) => (
       titleTemplate="%s | Lost Astronaut"
     >
       <html lang="de" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#ff6666"/>
+      <link rel="shortcut icon" href="/favicons/favicon.ico"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="msapplication-config" content="/favicons/browserconfig.xml"/>
+      <meta name="theme-color" content="#ff6666"/>
+
     </Helmet>
     <Header />
     <div className={style.content}>{children()}</div>
